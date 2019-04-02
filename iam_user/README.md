ba08bd0fb763df47ba21c8ec8ed28558d7e2e0de
Ayo Odeniyide <ayoola.odeniyide@verizondigitalmedia.com>
2019-04-01 23:19:35 -0700

    Initial commit

---
+++ iam_user/README.md
@@ -0,0 +1,5 @@
+# terraform-lab
+
+1) Install the latest version of terraform
+
+`wget https://releases.hashicorp.com/terraform/0.11.13/terraform_0.11.13_linux_amd64.zip`
---
+++ s3/s3_bucket.tf
@@ -0,0 +1,22 @@
+# Specify the provider and access details
+
+
+provider "aws" {
+  region = "${var.aws_region}"
+}
+
+
+resource "aws_s3_bucket" "lab" {
+  bucket = "${var.bucket_name}"
+  acl    = "private"
+
+
+  versioning {
+    enabled = true
+  }
+
+  tags = {
+    Name        = "${var.bucket_name}"
+    Environment = "lab"
+  }
+}
